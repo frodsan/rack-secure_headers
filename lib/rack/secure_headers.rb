e50eb950b74707aedfb10bf33fb8d01f71fd680c
Francesco Rodríguez <frodsan@me.com>
2015-09-21 20:39:00 +0200

    Minor refactor.

--- lib/rack/secure_headers.rb
+++ lib/rack/secure_headers.rb
@@ -10,15 +10,6 @@ module Rack
       x_xss_protection: "1; mode=block"
     }
 
-    DEFAULT_HEADERS = {
-      x_content_type_options: "X-Content-Type-Options",
-      x_frame_options: "X-Frame-Options",
-      x_permitted_cross_domain_policies: "X-Permitted-Cross-Domain-Policies",
-      x_xss_protection: "X-XSS-Protection"
-    }
-
-    HSTS_HEADER = "Strict-Transport-Security".freeze
-
     def initialize(app, options = {})
       @app = app
       @options = DEFAULTS.merge(options)
@@ -27,29 +18,39 @@ module Rack
     def call(env)
       status, headers, body = @app.call(env)
 
-      DEFAULT_HEADERS.each do |key, header|
-        headers[header] = @options[key] if @options[key]
-      end
+      add_base_headers(headers, @options)
 
-      if @options[:hsts]
-        headers[HSTS_HEADER] = Utils.hsts(@options[:hsts])
-      end
+      add_hsts_header(headers, @options[:hsts]) if @options[:hsts]
 
       return [status, headers, body]
     end
 
-    module Utils
-      HSTS_MAX_AGE = "max-age=%s".freeze
-      HSTS_INCLUDE_SUBDOMAINS = "; includeSubdomains".freeze
-      HSTS_PRELOAD = "; preload".freeze
+    private
 
-      def self.hsts(opts)
-        header = sprintf(HSTS_MAX_AGE, opts.fetch(:max_age))
-        header << HSTS_INCLUDE_SUBDOMAINS if opts[:include_subdomains]
-        header << HSTS_PRELOAD if opts[:preload]
+    BASE_HEADERS = {
+      x_content_type_options: "X-Content-Type-Options",
+      x_frame_options: "X-Frame-Options",
+      x_permitted_cross_domain_policies: "X-Permitted-Cross-Domain-Policies",
+      x_xss_protection: "X-XSS-Protection"
+    }
 
-        return header
+    def add_base_headers(headers, options)
+      BASE_HEADERS.each do |key, header|
+        headers[header] = options[key] if options[key]
       end
+    end
+
+    HSTS_HEADER = "Strict-Transport-Security".freeze
+    HSTS_MAX_AGE = "max-age=%s".freeze
+    HSTS_INCLUDE_SUBDOMAINS = "; includeSubdomains".freeze
+    HSTS_PRELOAD = "; preload".freeze
+
+    def add_hsts_header(headers, opts)
+      header = sprintf(HSTS_MAX_AGE, opts.fetch(:max_age))
+      header << HSTS_INCLUDE_SUBDOMAINS if opts[:include_subdomains]
+      header << HSTS_PRELOAD if opts[:preload]
+
+      headers[HSTS_HEADER] = header
     end
   end
 end
