f3b879c0c803065439b3d73167307cd3204ef6be
Francesco Rodríguez <frodsan@me.com>
2015-10-01 22:34:15 +0200

    Remove warnings.

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,5 @@
+$VERBOSE = true
+
 require "bundler/setup"
 require "cutest"
 require_relative "../lib/rack/secure_headers"
--- test/secure_headers.rb
+++ test/secure_headers.rb
@@ -1,5 +1,3 @@
-require_relative "helper"
-
 class App
   def call(env)
     return [200, {}, [""]]
